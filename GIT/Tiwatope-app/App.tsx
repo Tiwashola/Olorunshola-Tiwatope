3a3dea3b67f18e7773c3eafb84fb252d83392d42
Ayodotun-007 <ogunmodedeayodotun@gmail.com>
2026-04-24 00:45:34 +0100

    first commit

---
+++ GIT/Tiwatope-app/App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My name is Olorunshola Tiwatope,</Text>
+      <Text>and I'm a student of Computer Science</Text>
+      <Text>with the matric number of RUN/CMP/22/12978</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
